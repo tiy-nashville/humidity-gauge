698ab61afe8eb781ccaeb9c8c449c8b218a37ae6
Ryan Tablada <ryan.tablada@gmail.com>
2016-06-27 17:47:40 -0500

    Publish crash course

---
+++ app.js
@@ -0,0 +1,12 @@
+(function() {
+  const appId = '';
+  const input = document.querySelector('.weather-input');
+  const btn = document.querySelector('.weather-btn');
+  const list = document.querySelector('.weather-list');
+
+  function searchWeather(city) {
+
+  }
+
+  searchWeather('Nashville');
+})();
